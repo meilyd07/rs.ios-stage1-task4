c99c030012c741ee8fa83439709dd6388cae20b5
MeilyD07 <meilyd07@gmail.com>
2020-04-11 21:44:00 +0300

    feat: add decompose number task

--- RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
+++ RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
@@ -1,8 +1,37 @@
 #import "SquareDecomposer.h"
 
 @implementation SquareDecomposer
-- (NSArray <NSNumber*>*)decomposeNumber:(NSNumber*)number {
-    // good luck
-    return @[@0];
+- (NSMutableArray<NSNumber*>*)decomposeNumber:(NSInteger)current AndRemaining:(NSInteger)rest {
+    if (rest == 0) {
+        NSMutableArray <NSNumber*>* resultArray = [NSMutableArray new];
+        [resultArray addObject:@(current)];
+        return resultArray;
+    }
+    
+    //foreach number less than current
+    for (NSInteger i = current - 1; i > 0; i--){
+        if ((rest - i * i) >= 0) {
+            NSMutableArray <NSNumber*>* resultArray = [self decomposeNumber:i AndRemaining:(rest - i * i)];
+            
+            if (resultArray != nil) {
+                [resultArray addObject:@(current)];
+                return resultArray;
+            }
+        }
+    }
+    
+    return nil;
 }
+
+- (NSArray <NSNumber*>*)decomposeNumber:(NSNumber*)number {
+    
+    NSInteger intNumber = [number integerValue];
+    NSMutableArray<NSNumber*> *decomposeArray = [[self decomposeNumber:intNumber AndRemaining:(intNumber*intNumber)] mutableCopy];
+    if (decomposeArray != nil) {
+        [decomposeArray removeLastObject];
+        return decomposeArray;
+    }
+    return nil;
+}
+
 @end
